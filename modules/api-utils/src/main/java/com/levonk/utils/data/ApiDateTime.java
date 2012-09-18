d48b51ce45611bb079403643276ac6f8e3a0daa8
lk <git@lk.oib.com>
2012-09-18 02:09:39 -0700

    basic package

---
+++ modules/api-utils/src/main/java/com/levonk/utils/data/ApiDateTime.java
@@ -0,0 +1,8 @@
+package com.levonk.utils.data;
+
+public class ApiDateTime {
+	private int _timeEpochSeconds;
+	private int _timeZoneOffsetSeconds;
+	private String _timeZoneName;
+	private boolean _isDST;
+}
